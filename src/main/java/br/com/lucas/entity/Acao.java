0b8f0918730e876c136aa601bcdd43dcd9d1031c
Lucas Diniz <53830752+Akssasori@users.noreply.github.com>
2021-02-15 15:50:56 -0300

    adicionado cliente e programa

--- src/main/java/br/com/lucas/entity/Acao.java
+++ src/main/java/br/com/lucas/entity/Acao.java
@@ -2,7 +2,6 @@ package br.com.lucas.entity;
 
 import java.io.Serializable;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 import javax.persistence.Column;
@@ -14,85 +13,92 @@ import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.sun.istack.NotNull;
 
-
 @Entity
-@Table(name ="acao")
-public class Acao implements Serializable{
-	
+@Table(name = "acao")
+public class Acao implements Serializable {
+
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
 	@JsonProperty("id")
 	private Long id;
-	
-	@Column(name = "programa", length=100)
+
+	@Column(name = "programa", length = 100)
 	@JsonProperty("programa")
 	@NotNull
 	private String programa;
-	
+
 	@Column(name = "data")
 	@JsonProperty("data")
 	@JsonFormat(pattern = "dd-MM-yyyy")
 	private LocalDate data;
-	
-	@Column(name = "cliente", length=250)
+
+	@Column(name = "cliente", length = 250)
 	@JsonProperty("cliente")
 	private String cliente;
-	
+
 	@Column(name = "idCliente")
 	@JsonProperty("idCliente")
 	private Long idCliente;
-	
+
 	@Column(name = "produto", length = 100)
 	@JsonProperty("produto")
 	private String produto;
-	
+
 	@Column(name = "tipoAcao")
 	@JsonProperty("tipoAcao")
 	private String tipoAcao;
-	
+
 	@Column(name = "descricao", length = 250)
 	@JsonProperty("descricao")
 	private String descricao;
-	
+
 	@Column(name = "entrada")
 	@JsonProperty("entrada")
 	@JsonFormat(pattern = "HH:mm:ss")
 	private LocalTime entrada;
-	
+
 	@Column(name = "saida")
 	@JsonProperty("saida")
-	@JsonFormat(pattern = "HH:mm:ss")	
+	@JsonFormat(pattern = "HH:mm:ss")
 	private LocalTime saida;
-	
+
 	@Column(name = "agencia")
 	@JsonProperty("agencia")
 	private String agencia;
-	
-	@Column(name = "numeroVideo")
-	@JsonProperty("numeroVideo")
-	private Long numeroVideo;
-	
+
+//	@Column(name = "numeroVideo")
+//	@JsonProperty("numeroVideo")
+//	private Long numeroVideo;
+
+	@Column(name = "url")
+	@JsonProperty("url")
+	private String url;
+
 	@Column(name = "idAgencia")
 	@JsonProperty("idAgencia")
 	private Long idAgencia;
 	
-	@Column(name = "idSistema")
-	@JsonProperty("idSistema")
-	private Long idSistema;
-	
+	@Column(name = "duracao")
+	@JsonProperty("duracao")
+	@JsonFormat(pattern = "HH:mm:ss")
+	private LocalTime duracao;
+
+//	@Column(name = "idSistema")
+//	@JsonProperty("idSistema")
+//	private Long idSistema;
+
 	public Acao() {
-		
+
 	}
 
 	public Acao(Long id, String programa, LocalDate data, String cliente, Long idCliente, String produto,
-			String tipoAcao, String descricao, LocalTime entrada, LocalTime saida, String agencia, Long numeroVideo,
-			Long idAgencia, Long idSistema) {
+			String tipoAcao, String descricao, LocalTime entrada, LocalTime saida, String agencia, String url,
+			Long idAgencia) {
 		super();
 		this.id = id;
 		this.programa = programa;
@@ -105,17 +111,16 @@ public class Acao implements Serializable{
 		this.entrada = entrada;
 		this.saida = saida;
 		this.agencia = agencia;
-		this.numeroVideo = numeroVideo;
+		this.url = url;
 		this.idAgencia = idAgencia;
-		this.idSistema = idSistema;
 	}
 
 	@Override
 	public String toString() {
 		return "Acao [id=" + id + ", programa=" + programa + ", data=" + data + ", cliente=" + cliente + ", idCliente="
 				+ idCliente + ", produto=" + produto + ", tipoAcao=" + tipoAcao + ", descricao=" + descricao
-				+ ", entrada=" + entrada + ", saida=" + saida + ", agencia=" + agencia + ", numeroVideo=" + numeroVideo
-				+ ", idAgencia=" + idAgencia + ", idSistema=" + idSistema + "]";
+				+ ", entrada=" + entrada + ", saida=" + saida + ", agencia=" + agencia + ", url=" + url + ", idAgencia="
+				+ idAgencia + "]";
 	}
 
 	public Long getId() {
@@ -206,12 +211,12 @@ public class Acao implements Serializable{
 		this.agencia = agencia;
 	}
 
-	public Long getNumeroVideo() {
-		return numeroVideo;
+	public String getUrl() {
+		return url;
 	}
 
-	public void setNumeroVideo(Long numeroVideo) {
-		this.numeroVideo = numeroVideo;
+	public void setUrl(String url) {
+		this.url = url;
 	}
 
 	public Long getIdAgencia() {
@@ -222,23 +227,8 @@ public class Acao implements Serializable{
 		this.idAgencia = idAgencia;
 	}
 
-	public Long getIdSistema() {
-		return idSistema;
-	}
-
-	public void setIdSistema(Long idSistema) {
-		this.idSistema = idSistema;
-	}
-
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
-
-	
-	
-
-	
-
-	
 
 }
--- src/main/java/br/com/lucas/repository/AcaoRepository.java
+++ src/main/java/br/com/lucas/repository/AcaoRepository.java
@@ -1,11 +1,13 @@
 package br.com.lucas.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.lucas.entity.Acao;
-
+@Repository
 public interface AcaoRepository extends JpaRepository<Acao, Long> {
 
 }
+
 
 
